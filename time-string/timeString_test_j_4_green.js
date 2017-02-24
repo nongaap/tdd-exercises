9783b486fede19c5a36fc47842f4f38dd14f3d75
Mike Puangmalai <mike@nongaap.com>
2017-02-23 23:41:37 -0800

    timeString red/green tests 4

---
+++ time-string/timeString_test_j_4_green.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const chai = require('chai');
+const expect = chai.expect;
+
+// 4 green: Implement just enough logic to get the test passing
+
+const actual = [];
+const expected = [];
+
+function timeString(string) {
+	let hours = ['0'];
+	let other = string.split(':');
+
+	if(other.length > 2) {
+		return string;
+	} else {
+		return hours.concat(other).join(':');		
+	}
+}
+
+function test (input, output) {
+	actual.push(timeString(input));
+	expected.push(output);
+}
+
+test('00:01', '0:00:01');
+test('00:02', '0:00:02');
+test('00:03', '0:00:03');
+test('01:01', '0:01:01');
+test('1:00:01', '1:00:01');
+
+describe('timeString test', function() {
+
+  it('Returns expected string output when string is passed into timeString function', function() {
+    expect(actual).to.eql(expected);
+  });
+
+});
